6368ea0ad920d7d5fd6309308c539cdf605a2969
Kang Min <rkdals133@gmail.com>
2025-03-27 22:46:25 +0900

    test : 통합테스트 userId를 분리하여 테스트 독립성 보장

--- src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -7,7 +7,6 @@ import io.hhplus.tdd.dto.point.UseUserPointRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -20,9 +19,6 @@ import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@DirtiesContext(
-    classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD
-)
 public class PointServiceIntegrationTest {
 
     @Autowired
@@ -34,10 +30,16 @@ public class PointServiceIntegrationTest {
     @Autowired
     private PointLimit pointLimit;
 
+    private static Long userId = 0L;
+
+    private static Long getUserId() {
+        return userId++;
+    }
+
     @Test
     void 포인트를_충전하면_사용자의_포인트_현황을_반환한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
 
         // when
@@ -52,7 +54,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트_최대_정책을_넘는_금액를_충전하면_포인트_충전_불가_에러가_발생한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(pointLimit.max() + 1L);
 
         // when
@@ -66,7 +68,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트_충전_이후에_포인트_총액이_포인트_최대_정책을_넘으면_포인트_충전_불가_에러가_발생한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto1 = new ChargeUserPointRequestDto(100L);
         ChargeUserPointRequestDto chargeUserPointRequestDto2 = new ChargeUserPointRequestDto(pointLimit.max() - chargeUserPointRequestDto1.getAmount() + 1L);
 
@@ -84,7 +86,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_충전한_이후에_포인트_현황을_조회할_수_있다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
 
         UserPoint userPoint = pointService.chargeUserPoint(userId, chargeUserPointRequestDto);
@@ -100,7 +102,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_충전한적이_없는_유저가_포인트를_조회하면_유저_유효성_에러가_발생한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         // when
         RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> pointService.getUserPoint(userId));
 
@@ -112,7 +114,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_충전한_이후에_포인트를_사용할_수_있다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
         UseUserPointRequestDto useUserPointRequestDto = UseUserPointRequestDto.createdBy(100L);
 
@@ -129,7 +131,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_사용한_이후_잔액이_포인트_최소_정책보다_작으면_잔액_유효성_에러가_발생한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
         UseUserPointRequestDto useUserPointRequestDto = UseUserPointRequestDto.createdBy(chargeUserPointRequestDto.getAmount() + 1L);
 
@@ -147,7 +149,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_충전하면_사용자의_포인트_충전_히스토리를_조회할_수_있다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
 
         // when
@@ -165,7 +167,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트_충전하고_사용하면_사용자의_포인트_충전_사용_히스토리를_조회할_수_있다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         ChargeUserPointRequestDto chargeUserPointRequestDto = new ChargeUserPointRequestDto(100L);
         UseUserPointRequestDto useUserPointRequestDto = UseUserPointRequestDto.createdBy(100L);
 
@@ -188,7 +190,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 포인트를_충전하지_않은_사용자가_포인트_히스토리를_조회하면_유저_유효성_에러가_발생한다() {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
 
         // when
         RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> pointService.listPointHistory(userId));
@@ -201,7 +203,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 동일한_유저가_N번_포인트를_충전하면_N번의_포인트가_충전된다() throws InterruptedException {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         Long amount = 100L;
         int threadCount = 5;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -231,7 +233,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 동일한_유저가_N번_포인트를_충전할때_최대_포인트를_넘으면_이후_충전에_대해서_포인트_충전_불가_에러가_발생한다() throws InterruptedException {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         int threadCount = 5;
         int expectFailCount = 3;
 
@@ -272,7 +274,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 동일한_유저가_N번_포인트를_사용하면_N번의_포인트가_사용된다() throws InterruptedException {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         Long existPoint = 1000L;
 
         userPointTable.insertOrUpdate(userId, existPoint);
@@ -305,7 +307,7 @@ public class PointServiceIntegrationTest {
     @Test
     void 동일한_유저가_N번_포인트를_사용할때_잔액이_최소_포인트보다_작으면_이후_사용에_대해서_포인트_사용_불가_에러가_발생한다() throws InterruptedException {
         // given
-        Long userId = 1L;
+        Long userId = getUserId();
         int threadCount = 5;
         int expectFailCount = 3;
         Long existPoint = 1000L;
